27ad9c0b0bf047349b38575ef486f06a5b097064
av3n93rz <daniel.raffer@gmail.com>
2024-02-29 15:53:28 +0100

    manually add docs to enable adding metadata

--- backend/src/router/conversation.py
+++ backend/src/router/conversation.py
@@ -2,47 +2,41 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI, OpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain.memory import VectorStoreRetrieverMemory
-from langchain.chains import LLMChain, ConversationChain
-from langchain_core.documents import Document
+from langchain.chains import LLMChain
 from ..config import OPENAI_API_KEY, OPENAI_BASE_URL, POSTGRES_CONNECTION_STRING
 from ..memory import Memory
 from ..templates import default
-from langchain_core.runnables import RunnableConfig
+import datetime
 
 router = APIRouter()
 
 
 class ConversationRequest(BaseModel):
     message: str
+    user_id: str
+    conversation_id: str
 
 
 class ConversationResponse(BaseModel):
     message: str
 
 
-""" memory: VectorStoreRetrieverMemory = Memory.get_inmemory_vectorstore_memory(
-    embedding_size=1536, k=10
-) """
-
-
 @router.post("/conversation")
-async def conversation(request: ConversationRequest) -> ConversationResponse:
+def conversation(request: ConversationRequest) -> ConversationResponse:
     output_parser = StrOutputParser()
     template = default.DEFAULT_TEMPLATE
+    prompt = PromptTemplate(input_variables=["history", "input"], template=template)
+
+    collection_name = request.user_id + request.conversation_id
     memory: VectorStoreRetrieverMemory = Memory.get_pgvector_memory(
         connection_string=POSTGRES_CONNECTION_STRING,
-        collection_name="UserID+ConversationId",
+        collection_name=collection_name,
         k=10,
     )
-    """ 
-    res = await memory.retriever.aadd_documents(
-        [Document(page_content="asd:)", metadata={"name": "Avi"})]
-    )
-    print("IDDDDSS", res) """
 
-    prompt = PromptTemplate(input_variables=["history", "input"], template=template)
+    relevant_docs = memory.retriever.get_relevant_documents(request.message)
 
     llm = ChatOpenAI(
         model="gpt-3.5-turbo",
@@ -50,8 +44,25 @@ async def conversation(request: ConversationRequest) -> ConversationResponse:
         openai_api_key=OPENAI_API_KEY,
     )
 
-    chain = LLMChain(llm=llm, output_parser=output_parser, memory=memory, prompt=prompt)
+    chain = LLMChain(llm=llm, output_parser=output_parser, prompt=prompt)
 
-    output = await chain.ainvoke(input=request.message, return_only_outputs=True)
-    # response = chain.predict(input=request.message)
-    return ConversationResponse(message=output["text"])
+    try:
+        output = chain.invoke(
+            input={"input": request.message, "history": relevant_docs},
+            return_only_outputs=True,
+        )
+
+        # Save to memory
+        new_docs = memory._form_documents(
+            inputs={"input": request.message}, outputs=output
+        )
+
+        ct = datetime.datetime.now()
+        new_docs[0].metadata = {"created_at": ct.timestamp()}
+        memory.retriever.add_documents(new_docs)
+
+        # Return response
+        return ConversationResponse(message=output["text"])
+
+    except BaseException as e:
+        raise e
